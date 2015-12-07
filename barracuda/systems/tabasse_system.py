83b91a07be0c2893a9e4e54f93aaa2f7d3a7d2d8
Vincent <vincent.mezino@gmail.com>
2015-12-07 17:43:14 +0100

    TabasseSystem do anything if no target is setted. Correct 'death' notification

--- barracuda/systems/tabasse_system.py
+++ barracuda/systems/tabasse_system.py
@@ -9,17 +9,21 @@ class TabasseSystem(System):
     def update(self, dt):
         for entity, behavior in self.entity_manager.pairs_for_type(
                 BehaviorComponent):
-            target_status = self.entity_manager.component_for_entity(
-                behavior._target, StatusComponent)
-            if target_status.dead:
-                continue
-            if behavior._action == BehaviorActions.hit:
-                target_status.health -= 5
-                if target_status.health <= 0:
-                    self.dead = True
-                    target_status.health = 0
-            elif behavior._action == BehaviorActions.heal:
-                target_status.health += 5
-                if target_status.health > 100:
-                    target_status.health = 100
-            behavior.reset()
+            status = self.entity_manager.component_for_entity(
+                entity, StatusComponent)
+            if behavior._target:
+                target_status = self.entity_manager.component_for_entity(
+                    behavior._target, StatusComponent)
+                if target_status.dead:
+                    continue
+                if behavior._action == BehaviorActions.hit:
+                    target_status.health -= 5
+                    if target_status.health <= 0:
+                        target_status.dead = True
+                        target_status.health = 0
+                        print("%s est mort" % target_status.name)
+                elif behavior._action == BehaviorActions.heal:
+                    target_status.health += 5
+                    if target_status.health > 100:
+                        target_status.health = 100
+                behavior.reset()
